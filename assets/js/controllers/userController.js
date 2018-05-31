25feabe64ea618422d635d61783dbf4361f69dae
Israel <israel@overseasinternational.com>
2018-05-31 11:03:01 -0400

    Geting json

--- assets/js/controllers/userController.js
+++ assets/js/controllers/userController.js
@@ -1,36 +1,45 @@
-everyMundo.controller('userController',['$scope', 'userService', function ($scope, userService) {
+everyMundo.controller('userController', ['$scope', 'userService', function ($scope, userService) {
 
     $scope.sortKey = 'id';
 
     //Init with all my users
-    $scope.users = userService.allUsers();
+
+    userService.allUsers().then(function (data) {
+        $scope.users = data;
+    });
+
 
     //This object going to contain all users id was selected
     $scope.selected = {};
 
 
     //Function to return if there are any element selected to delete
-    $scope.elementSelected = function(){
-      return Object.values($scope.selected).indexOf(true) > -1 ? false : true;
-
-  }
-
-    $scope.sort = function(keyname){
-        $scope.sortKey = keyname;
-        $scope.reverse = !$scope.reverse;
-    }
-
-
-    $scope.showFemale = function(gender){
-
-        $scope.users = gender != "All" ? userService.usersFemale(gender) : userService.allUsers();
+    $scope.elementSelected = function () {
+        return Object.values($scope.selected).indexOf(true) > -1 ? false : true;
 
     };
 
+    $scope.sort = function (keyname) {
+        $scope.sortKey = keyname;
+        $scope.reverse = !$scope.reverse;
+    };
 
-    $scope.deleteUsers = function(user_id){
 
-        $scope.users = userService.deleteUsers($scope.selected , user_id);
+    $scope.showFemale = function (gender) {
+
+        if(gender != "All"){
+            userService.usersFemale(gender).then(function (data) {
+                $scope.users = data;
+            });
+        }
+    };
+
+
+    $scope.deleteUsers = function (user_id) {
+
+        userService.deleteUsers($scope.selected, user_id).then(function(data){
+            $scope.users = data;
+        });
     }
 
 }]);
